392e9ffa5fd479c80c3f844dc4f35659d91d74bf
James Lindfors <jameslindfors@outlook.com>
2024-05-04 14:17:06 -0700

    Adds the http request to the charger data api and creates the view and adapter for a recycler view list of chargers

--- app/src/main/java/com/cst338/project02/ChargerList.java
+++ app/src/main/java/com/cst338/project02/ChargerList.java
@@ -1,24 +1,78 @@
 package com.cst338.project02;
 
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.cst338.project02.databinding.ActivityChargerListBinding;
+import com.cst338.project02.databinding.ActivityLandingPageBinding;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChargerList extends AppCompatActivity {
+
+    ActivityChargerListBinding binding;
+    ArrayList<ChargerRowModel> chargerRowModels = new ArrayList<>();
+    String url =
+            "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest?format=json&location=93933&api_key=Wo7p9LICDqpQNQaaU1bOX4h8iiFlDPuuMwCGjfdG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_charger_list);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        binding = ActivityChargerListBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+
+        setContentView(view);
+
+        RecyclerView recyclerView = findViewById(R.id.chargerDataList);
+        setUpChargerRowModels();
+
+        ChargerListRow_Adapter adapter = new ChargerListRow_Adapter(this, chargerRowModels);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        JsonObjectRequest req = new JsonObjectRequest(Request.Method.GET ,url, null,
+                new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject jsonObject) {
+                try {
+                    JSONArray stations = jsonObject.getJSONArray("fuel_stations");
+                    String x = stations.optJSONObject(0).get("station_name").toString();
+
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+
+            }
         });
+
+        Volley.newRequestQueue(this).add(req);
+    }
+
+    private void setUpChargerRowModels() {
+       String[] chargerName =  {"Charger 1", "Charger 2"};
+       String[] chargerLocations = {"Marina, CA", "Monterey, CA"};
+
+       for (int i = 0; i < chargerName.length; i++) {
+           chargerRowModels.add(new ChargerRowModel(chargerName[i], chargerLocations[i]));
+       }
     }
 }
---
+++ app/src/main/java/com/cst338/project02/ChargerListRow_Adapter.java
@@ -0,0 +1,57 @@
+package com.cst338.project02;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class ChargerListRow_Adapter extends RecyclerView.Adapter<ChargerListRow_Adapter.ViewHolder> {
+
+    Context context;
+    ArrayList<ChargerRowModel> chargerRowModels;
+
+    public ChargerListRow_Adapter(Context context, ArrayList<ChargerRowModel> chargerRowModelList) {
+        this.context = context;
+        this.chargerRowModels = chargerRowModelList;
+    }
+
+    @NonNull
+    @Override
+    public ChargerListRow_Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.charger_list_row, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ChargerListRow_Adapter.ViewHolder holder, int position) {
+        holder.chargerName.setText(chargerRowModels.get(position).getStationName());
+        holder.chargerLocation.setText(chargerRowModels.get(position).getStationLocation());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return chargerRowModels.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView chargerName;
+        TextView chargerLocation;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            chargerName = itemView.findViewById(R.id.stationNameView);
+            chargerLocation = itemView.findViewById(R.id.stationLocationView);
+        }
+    }
+}
