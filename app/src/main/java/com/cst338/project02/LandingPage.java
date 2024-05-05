921a287160e9c5244c7af603f8714b773ed9691c
Jashan Rahal <93688148+Jashan66@users.noreply.github.com>
2024-05-04 17:40:27 -0700

    Added profile /create charger page w/ navigation (#8)

--- app/src/main/java/com/cst338/project02/LandingPage.java
+++ app/src/main/java/com/cst338/project02/LandingPage.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+
 
 import com.cst338.project02.databinding.ActivityLandingPageBinding;
 
@@ -24,6 +26,31 @@ public class LandingPage extends AppCompatActivity {
         setContentView(view);
 
         binding.userName.setText(getIntent().getStringExtra("USERNAME"));
+
+        binding.navigation.setOnItemSelectedListener(item -> {
+            Intent intent;
+            if (item.getItemId() == R.id.navigation_home) {
+                intent = new Intent(this, LandingPage.class);
+                startActivity(intent);
+            } else if (item.getItemId() == R.id.navigation_profile) {
+                intent = new Intent(this, ProfilePage.class);
+                startActivity(intent);
+            } else if (item.getItemId() == R.id.navigation_create){
+                intent = new Intent(this, AddChargerPage.class);
+                startActivity(intent);
+            }
+
+            return true;
+        });
+
+        binding.goProfilePage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goProfilePage = new Intent(LandingPage.this, ProfilePage.class);
+                startActivity(goProfilePage);
+            }
+        });
+
 
 
         if (!canAccessLocation()) {
