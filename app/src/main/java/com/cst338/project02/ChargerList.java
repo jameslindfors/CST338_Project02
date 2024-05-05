1981f0c28450edfd1245f716591b28c79810eb00
James Lindfors <jameslindfors@outlook.com>
2024-05-05 14:27:20 -0700

    Reformats the charger list view and row and adds an icon button to go to charger details

--- app/src/main/java/com/cst338/project02/ChargerList.java
+++ app/src/main/java/com/cst338/project02/ChargerList.java
@@ -1,6 +1,7 @@
 package com.cst338.project02;
 
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,18 +14,16 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.cst338.project02.databinding.ActivityChargerListBinding;
-import com.cst338.project02.databinding.ActivityLandingPageBinding;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class ChargerList extends AppCompatActivity {
 
     ActivityChargerListBinding binding;
+    ArrayList<Pair<String, String>> stationDetails = new ArrayList<>();
     ArrayList<ChargerRowModel> chargerRowModels = new ArrayList<>();
     String url =
             "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest?format=json&location=93933&api_key=Wo7p9LICDqpQNQaaU1bOX4h8iiFlDPuuMwCGjfdG";
@@ -37,42 +36,44 @@ public class ChargerList extends AppCompatActivity {
         View view = binding.getRoot();
 
         setContentView(view);
+        requestChargerData();
+    }
+
+    private void setUpChargerRowModels() {
+        for (int i = 0; i < stationDetails.size(); i++) {
+            chargerRowModels.add(new ChargerRowModel(stationDetails.get(i).first, stationDetails.get(i).second));
+        }
 
         RecyclerView recyclerView = findViewById(R.id.chargerDataList);
-        setUpChargerRowModels();
-
         ChargerListRow_Adapter adapter = new ChargerListRow_Adapter(this, chargerRowModels);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
 
-        JsonObjectRequest req = new JsonObjectRequest(Request.Method.GET ,url, null,
+    private void requestChargerData() {
+        JsonObjectRequest req = new JsonObjectRequest(Request.Method.GET, url, null,
                 new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject jsonObject) {
-                try {
-                    JSONArray stations = jsonObject.getJSONArray("fuel_stations");
-                    String x = stations.optJSONObject(0).get("station_name").toString();
-
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }, new Response.ErrorListener() {
+                    @Override
+                    public void onResponse(JSONObject jsonObject) {
+                        try {
+                            JSONArray stations = jsonObject.getJSONArray("fuel_stations");
+                            for (int i = 0; i < stations.length(); i++) {
+                                JSONObject obj = stations.getJSONObject(i);
+                                String stationLoc = obj.get("street_address").toString()
+                                        + " " + obj.get("city") + " " + obj.get("zip");
+                                stationDetails.add(new Pair<String, String>(obj.get("station_name").toString(), stationLoc));
+                            }
+                            setUpChargerRowModels();
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError volleyError) {
-
             }
         });
 
         Volley.newRequestQueue(this).add(req);
-    }
-
-    private void setUpChargerRowModels() {
-       String[] chargerName =  {"Charger 1", "Charger 2"};
-       String[] chargerLocations = {"Marina, CA", "Monterey, CA"};
-
-       for (int i = 0; i < chargerName.length; i++) {
-           chargerRowModels.add(new ChargerRowModel(chargerName[i], chargerLocations[i]));
-       }
     }
 }
