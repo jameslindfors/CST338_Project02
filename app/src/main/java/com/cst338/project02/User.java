687cb93d2c3478dd16c8a81cb62306fafdf33389
Jashan Rahal <jashanrahal6@gmail.com>
2024-05-04 17:46:25 -0700

    updated user class w/ getters & setters

--- app/src/main/java/com/cst338/project02/User.java
+++ app/src/main/java/com/cst338/project02/User.java
@@ -27,6 +27,35 @@ public class User {
         this.isAdmin = isAdmin;
     }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
 }
